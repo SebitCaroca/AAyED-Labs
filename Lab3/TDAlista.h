a64a06b224eacf36d56937963e2765e144960b35
SebitCaroca <102820453+SebitCaroca@users.noreply.github.com>
2022-06-06 05:03:49 -0400

    Lab3 update

---
+++ Lab3/TDAlista.h
@@ -0,0 +1,177 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//TDA Lista Enlazada--------------------------------------------------------------------------------
+
+typedef struct nodoGenerico{
+   int dato;
+   struct nodoGenerico* siguiente;
+}nodo;
+
+typedef struct listaGen{
+    nodo* inicio;
+}TDAlista;
+
+//Operadores---------------------------------------------------------------------------------------
+
+// Dom :
+// Rec : TDA Lista Enlazada
+// Desc: Crea una lista enlazada vacia, preparada para agregar elementos.
+TDAlista* crearListaVacia(){
+  TDAlista* lista=(TDAlista*)malloc(sizeof(TDAlista));
+  lista->inicio=NULL;
+  return lista;
+}
+
+// Dom :
+// Rec : TDA Nodo
+// Desc: Crea un nodo completamente vacio, preparado para agregar elementos.
+nodo* crearNodoVacio(){
+  nodo* nodoNuevo = (nodo*)malloc(sizeof(nodo));
+  nodoNuevo->dato = 0;
+  nodoNuevo->siguiente = NULL;
+  return nodoNuevo;
+}
+
+// Dom : TDA Lista Enlazada
+// Rec : Int
+// Desc: Revisa si la lista enlazada esta vacia, retornando 1 si es asi, 0 si no.
+int esListaVacia(TDAlista* lista){
+    if (lista->inicio == NULL){ return 1; }
+    else{ return 0; }
+}
+
+// Dom: TDA Lista Enlazada, TDA Nodo
+// Rec:
+// Desc: Agrega un nodo nuevo hacia el final de una lista.
+void insertarNodoFinal(TDAlista* lista, nodo* newNodo){
+  if (esListaVacia(lista)){
+    lista->inicio = newNodo;
+  }else{
+    nodo* aux = lista->inicio;
+    while (aux->siguiente != NULL){
+      aux = aux->siguiente;
+    }
+    aux->siguiente = newNodo;
+  }
+}
+
+// Dom: TDA Lista Enlazada, TDA Nodo
+// Rec:
+// Desc: Agrega un nodo nuevo hacia el final de una lista.
+void insertarNodoInicio(TDAlista* lista, nodo* newNodo){
+  nodo* aux = lista->inicio;
+  newNodo->siguiente = aux;
+  lista->inicio = newNodo;
+}
+
+// Dom : TDA Lista Enlazada
+// Rec : 
+// Desc: Remueve el primer nodo de la lista enlazada.
+void eliminarInicio(TDAlista* lista){
+  nodo* aux;
+  if(!esListaVacia(lista)){
+    aux = lista->inicio;
+    lista->inicio = lista->inicio->siguiente;
+    free(aux);
+  }
+}
+
+// Dom : TDA Lista Enlazada
+// Rec : 
+// Desc: Remueve todos los nodos de la lista enlazada, empezando por el primero.
+void liberarLista(TDAlista* lista){
+  while (esListaVacia(lista) == 0){
+    eliminarInicio(lista);
+  }
+}
+
+// Dom : nodo
+// Rec :
+// Desc: Muestra en la consola los contenidos de un nodo.
+void printNodo(nodo* nodoLista){
+  if (nodoLista == NULL){
+    printf("Nodo no existe.\n");
+  }else{
+    printf("Dato: %d",nodoLista->dato);
+    if (nodoLista->siguiente == NULL){
+      printf(", Este es el ultimo valor.\n");
+    }else{
+      printf(".\n");
+    }
+  }
+}
+
+// Dom : TDA Lista Enlazada
+// Rec : 
+// Desc: Exactamente lo mismo que printNodo, pero es para toda la lista enlazada.
+void printLista(TDAlista* lista){
+  if (!esListaVacia(lista)){
+    nodo* aux=lista->inicio;
+    int i=1;
+    while (aux != NULL){
+      printf("Nodo %d\n",i);
+      printNodo(aux);
+      i++;
+      aux = aux->siguiente;
+    }
+  }
+  else
+    printf("La lista esta vacia\n");
+}
+
+// Dom : TDA Lista Enlazada
+// Rec : int
+// Desc: Devuelve la cantidad de nodos que existen en la lista enlazada actualmente
+int cantidadLista(TDAlista* lista){
+  int i=0;
+  nodo* aux = lista->inicio;
+  while (aux != NULL){
+    i++;
+    aux=aux->siguiente;
+  }
+  return i;
+}
+
+// Dom: TDA Lista Enlazada, value
+// Rec:
+// Desc: Remueve el nth nodo de la lista enlazada.
+void eliminarNth(TDAlista* lista,int value){
+  if (esListaVacia(lista) || value > cantidadLista(lista) || value <= 0){
+    //No hacer nada si value esta fuera de rango.
+  }else if (value == 1){
+    nodo* aux = lista->inicio;
+    lista->inicio = aux->siguiente;
+    free(aux);
+  }else{
+    int i;
+    nodo* aux = lista->inicio;
+    nodo* auxDel = lista->inicio;
+
+    for (i = 2; i < value; i++){
+      aux = aux->siguiente;
+    }
+    auxDel = aux->siguiente;
+    aux->siguiente = auxDel->siguiente;
+    free(auxDel);
+  }
+}
+
+// Dom : TDA Lista Enlazada, int
+// Rec : NULL | nodo
+// Desc: Devuelve un nodo de la lista dependiendo de la posicion de este.
+//       NOTA: La cantidad de nodos empieza desde el 1 con esta funcion.
+//       Asi que si el valor esta fuera de rango, retornara NULL.
+nodo* obtenerNodo(TDAlista* lista, int pos){
+  if ((pos > cantidadLista(lista)) || (pos < 1) ){
+    return NULL;
+  }else{
+    int i;
+    nodo* aux = lista->inicio;
+    for (i = 0; i < pos-1; i++){
+      aux = aux->siguiente;
+    }
+    return aux;
+  }
+}
---
+++ Lab3/main.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "TDAlista.h"
+#include "TDApila.h"
+#include "TDAgrafo-edit.h"
+
+int extraerMinimo(int* d_longitud,int* d_visitado,int cvertices){
+	int minimo = INT_MAX;
+	int indiceMinimo = 0;
+	int i;
+	for (i = 0; i < cvertices; ++i){
+		//Obtener la minima distancia que no haya sido visto aun.
+		if (d_visitado[i] == 0){
+			if ( (d_longitud[i] < minimo) && (d_longitud[i] != 0) ){
+				minimo = d_longitud[i];
+				indiceMinimo = i;
+			}
+		}
+	}
+	
+	return indiceMinimo;
+}
+
+//I: Array, Int
+//O: Bool (Int)
+//Revisa si queda algun 0 en el arreglo visitados.
+//Si es asi, retorna 1, sino 0.
+int quedanSinVisitar(int* d_visitado, int limite){
+  for (int i = 0; i < limite ; i++){
+    if (d_visitado[i] == 0){ return 1; }
+  }
+  return 0;
+}
+
+/*
+La idea que tengo actualmente es la siguiente...
+Definamos X como nodos que NO pertenecen a las estaciones de bomberos
+--(Implica que Xi es un nodo que pertenece a X)
+Definamos Y como nodos que pertenecen a las estaciones de bomberos
+--(Implica que Yi es un nodo que pertenece a Y)
+
+Procedimiento:
+--Repetir lo siguiente hasta que todos los nodos de X hayan sido visitados:
+----Obtener la distancia mas corta entre Xi y un nodo de Y (Dijkstra sera util)
+----Si hay multiples nodos de Y
+------Probar con todos los Yi
+------Obtener el mas corto
+----Despues de probar con todos los nodos
+----Escribir en cuartel.out el nodo cuyo valor es el mas alto del arreglo
+*/
+void dijkstra(TDAgrafo* grafo,int vertice){
+	//Inicializacion.
+	int* d_anterior = (int*)calloc(sizeof(int),grafo->cvertices);
+	int* d_visitado = (int*)calloc(sizeof(int),grafo->cvertices);
+	int* d_longitud = (int*)calloc(sizeof(int),grafo->cvertices);
+	int u;
+  	int s = vertice-1;
+	TDAlista* listaAdy;
+	nodo* aux;
+	int w,i;
+
+	//Algoritmo.
+	for (w = 0; w < grafo->cvertices ; w++){
+		d_anterior[w] = -1; //NULL
+		d_visitado[w] = 0;  //FALSO
+		if ( grafo->adyacencias[s][w] > 0 ){
+			d_longitud[w] = grafo->adyacencias[s][w];
+			d_anterior[w] = s;
+		}else{
+			d_longitud[w] = INT_MAX; //INFINITO
+		}
+	}
+  
+	d_longitud[s] = 0;
+	d_visitado[s] = 1; //VERDADERO
+	while( quedanSinVisitar(d_visitado , grafo->cvertices) ){
+		u = extraerMinimo(d_longitud , d_visitado , grafo->cvertices);
+		d_visitado[u] = 1;
+		listaAdy = obtenerAdyacentes(grafo,u);
+		aux = listaAdy->inicio;
+		while(aux != NULL){
+			if (d_longitud[aux->dato] > d_longitud[u] + grafo->adyacencias[u][aux->dato] ){
+				d_longitud[aux->dato] = d_longitud[u] + grafo->adyacencias[u][aux->dato];
+				d_anterior[aux->dato] = u;
+			}
+			aux=aux->siguiente;
+		}
+	}
+
+  for (i = 0; i < grafo->cvertices ; i++){
+    printf("%d %d %d\n",d_anterior[i],d_visitado[i],d_longitud[i]);
+  }
+}
+
+      
+int main(void) {
+  FILE* archivo = fopen("pueblo1.in","r");
+  TDAgrafo* grafo = leerGrafoNoDirigidoPonderado(archivo);
+  imprimirMatrizAdyacencia(grafo);
+  fclose(archivo);
+  dijkstra(grafo,5);
+  printf("Fin.\n");
+  return 0;
+}
